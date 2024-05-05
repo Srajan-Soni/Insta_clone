8150dd44a69dfb664060daf53b34141ad8b31b2d
Srajan-Soni <60183046+Srajan-Soni@users.noreply.github.com>
2024-05-05 19:57:56 +0530

    first commit

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoUrl: "mongodb+srv://srajansoni400:Srajan2000@cluster0.cgvqjmd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster",
+    Jwt_secret: "faslkfocvneofu"
+
+};
